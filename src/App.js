5cf548aed5bcfb82ae34f62d2fd1d71fd9bc27ed
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-02-28 13:09:55 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      This is netArt APP
+    <div className='h-[100vh] w-[50vw] mx-auto bg-gradient-to-b from-[#fefff3] to-[#fbfcad]'>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <img src='./asserts/logo.png' alt='Logo' className='w-[14vw] mx-auto mt-2'></img>
+    </div>
+  )
+}
+
+export default Header
