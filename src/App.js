bbd2ed68ece278ccacffee66725cb77d0ddd234d
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-02-28 14:17:17 +0530

    footer added , web app complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import SectionI from './components/SectionI';
 import SectionII from './components/SectionII';
 
 function App() {
   return (
-    <div className='h-fit pb-1 w-[50vw] mx-auto bg-gradient-to-b from-[#fefff3] to-[#fbfcad]'>
+    <div className='h-fit w-[50vw] mx-auto bg-gradient-to-b from-[#fffffa] to-[#feffb3]'>
       <Header/>
       <SectionI/>
       <SectionII/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { IoCallOutline } from "react-icons/io5";
+import { FaFacebook } from "react-icons/fa";
+import { FaGlobe } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className='w-full p-2'>
+
+    <div className='w-full border-t-2 border-red-500 flex flex-col items-center text-[11px] mx-auto'>
+
+        <p className='font-bold mt-2'>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</p>
+        <p className='font-semibold mt-2 text-center'>CHEMICALS & PROCESS <span className='text-red-700'> |</span> POWER <span className='text-red-700'> |</span> WATER & WASTE WATER<span className='text-red-700'> |</span>  OILS & GAS <span className='text-red-700'> |</span>  PHARMA<span className='text-red-700'> |</span> SUGARS & DISTILLERIES<span className='text-red-700'> |</span> PAPER & PULP<span className='text-red-700'> |</span> MARINE & DEFENCE<span className='text-red-700'> |</span> METAL & MINING<span className='text-red-700'> |</span> FOOD & BEVERAGE<span className='text-red-700'> |</span> PETROCHEMICAL & REFINERIES<span className='text-red-700'> |</span> SOLAR<span className='text-red-700'> |</span> BUILDING<span className='text-red-700'> |</span> HVAC<span className='text-red-700'> |</span> FIRE FIGHTING<span className='text-red-700'> |</span> AGRICULTURE & RESIDENTIAL</p>
+        <div className='flex justify-around bg-red-500 w-full h-[7vh] p-3 mt-3'>
+            <div className='flex w-fit'>
+                <IoCallOutline size={28} className='text-black bg-red-500 rounded-full p-1' />
+                <p className='text-white mt-2 ml-1'>Toll free <strong>1800 200 1234</strong></p>
+            </div>
+            <div className='flex w-fit'>
+                <FaFacebook size={22} />
+                <p className='text-white mt-2 ml-1'>www.facebook.com/cripumps</p>
+            </div>
+            <div className='flex w-fit'>
+                <FaGlobe size={22} />
+                <p className='text-white mt-2 ml-1'>www.crigroups.com</p>
+            </div>
+        </div>
+    </div>
+
+      
+    </div>
+  )
+}
+
+export default Footer
